4f9a77736de125d59001abe32753960049c27be1
ArLE <arlequin.prx@gmail.com>
2014-06-08 10:56:23 +0900

    First commit: make files

---
+++ First_Shooting_Game/First_Shooting_Game/WinMain.cpp
@@ -0,0 +1,15 @@
+#include "DxLib.h"
+
+// WinMainä÷êî
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	ChangeWindowMode(TRUE);
+
+	if (DxLib_Init() == -1)
+		return -1;
+	SetGraphMode(640, 480, 16);
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	DxLib_End();
+	return 0;
+}
